13c82185c4135d6effde1f10e62616570585a3c1
Julien Sanchez <sanchezjulien05@gmail.com>
2026-03-26 15:59:03 +0100

    heritage

--- SRC/Objets/Taser.js
+++ SRC/Objets/Taser.js
@@ -1,6 +1,10 @@
-class Taser {
-    constructor(){
-        this.name = "Taser";
-        this.chance = 0.5; //chance de toucher de 50%
-    }
+// herite de weapom + c'est attribue 
+import Weapon from "./weapon.js";
+
+export default class Taser extends Weapon {
+  constructor() {
+    super("Taser", 0);      // super pour donne l heritage et pas this pour definir 
+    // degat à adapter
+    this.chance = 0.5;      // this pour attribuer //50% de toucher
+  }
 }
