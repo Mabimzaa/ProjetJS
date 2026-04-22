c6460cac638e6d2ba19ad60ecc9c916e5a4ba66d
Julien Sanchez <sanchezjulien05@gmail.com>
2026-04-22 17:16:51 +0200

    fonction cree

---
+++ fonction.js
@@ -0,0 +1,38 @@
+function drawBox(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, boxSize, boxSize);
+}
+
+function spawnFood() {
+    return {
+        x: Math.floor(Math.random() * (width / boxSize)) * boxSize,
+        y: Math.floor(Math.random() * (height / boxSize)) * boxSize
+    }
+}
+
+let food = spawnFood();
+document.addEventListener("keydown", changeDirection);
+function changeDirection(e){
+
+    if(e.key === "ArrowLeft" && direction !== "RIGHT") {
+        direction = "LEFT";
+    } else if(e.key === "ArrowUp" && direction !== "DOWN") {
+        direction = "UP";
+    } else if(e.key === "ArrowRight" && direction !== "LEFT") {
+        direction = "RIGHT";
+    } else if(e.key === "ArrowDown" && direction !== "UP") {
+        direction = "DOWN";
+    }
+}
+
+function colisionWithBody(head, body) {
+
+    for(let i = 0; i < body.length; i++) {
+        if(head.x === body[i].x && head.y === body[i].y)
+        return true;
+        }
+}
+
+//fonction principale drawGame
+
+// lance la boucle 
