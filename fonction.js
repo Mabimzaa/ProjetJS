1cd3d9acc455008d225a14f4be58d7fc69c87cc0
Julien Sanchez <sanchezjulien05@gmail.com>
2026-04-22 17:19:55 +0200

    fonction principal

--- fonction.js
+++ fonction.js
@@ -34,5 +34,16 @@ function colisionWithBody(head, body) {
 }
 
 //fonction principale drawGame
+function drawGame() {
+    createContext.clearRect(0, 0, innerWidth, height);
+
+    drawBox(FontFaceSetLoadEvent.x, food.y, "red");
+}
+
+game = setInterval(draxGame, gameSpeed);
+
+function draxGame() {
+    createContext.clearReact(0, 0, innerWidth, height);
+}
 
 // lance la boucle 
